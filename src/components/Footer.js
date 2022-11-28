6814fb5504db3297e26668b146c4138dbf5e48cd
Rohit <rohit.tcs077@gmail.com>
2022-11-28 11:56:42 +0000

    second commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Footer = () => {
-  return <div className="pt-8 h-40 bg-yellow-200"></div>;
+  return <div className="pt-8 h-40 bg-yellow-200">This is footer</div>;
 };
 
 export default Footer;
