ba3538ff341e867bf4e41c753f2e4e6794d33b7e
Rohit <rohit.tcs077@gmail.com>
2022-11-29 15:31:10 +0000

    Added footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,16 @@
 import React from "react";
+import DevoloperContact from "./DevoloperContact";
 
 const Footer = () => {
   return (
-    <div className="pt-8 h-40 bg-yellow-200">
-      <p>Newsletter</p>
+    <div className="pt-8 bg-black text-white">
+      <div className="flex text-sm justify-evenly">
+        <h4>FAQ</h4>
+        <h4>Review</h4>
+        <h4>Others</h4>
+      </div>
+      <DevoloperContact />
+      {/* devoloper image */}
     </div>
   );
 };
--- src/components/Product.js
+++ src/components/Product.js
@@ -3,7 +3,11 @@ import React from "react";
 const Product = ({ productimg, text }) => {
   return (
     <div className="pt-12">
-      <img src={productimg} alt="" className="sm:w-72" />
+      <img
+        src={productimg}
+        alt=""
+        className="sm:w-72 w-[28rem] justify-center flex"
+      />
       <h1>{text}</h1>
     </div>
   );
